2c4632ed2f1d44a3c6375a6cf2fd6b703f5bf612
Jair <angeljair354@gmail.com>
2026-08-19 18:52:49 -0600

    Prueba de boton e input

--- pages/2_hola.py
+++ pages/2_hola.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title("Esta es una pagina nueva")
+
+nombre = st.text_input("Ingresa tu nombre")
+
+if nombre:
+    st.write(f"Mucho gusto {nombre}")
+
+if st.button("Da un click"):
+    st.success("Gracias por precionar mi boton :)")
