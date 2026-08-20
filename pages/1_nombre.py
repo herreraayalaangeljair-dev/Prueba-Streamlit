e98b16fed5646520aeaa5422235b174d16c4e3e5
Jair <angeljair354@gmail.com>
2026-08-19 19:26:02 -0600

    Seccion de analisis de oracion

--- pages/1_nombre.py
+++ pages/1_nombre.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
-st.title("Esta es una pagina nueva")
+st.set_page_config(page_title="Analizador sencillo de texto", page_icon="🥵",layout="centered")
+
+st.title("Aquí vas a ingresar un string y recibir un análisis sencillo.")
+
+texto_de_analisis = st.text_area("Ingresa un texrto cualquiera", placeholder="Ej. Mucho gusto, mi nombre es...")
+
+if texto_de_analisis:
+    palabras = texto_de_analisis.split()
+    numero_palabras = len(palabras)
+    numero_caracteres = len(texto_de_analisis)
+
+    st.markdown("----------------------------------------------")
+    st.subheader("Resultados :)")
+
+    columna1, columna2 = st.columns(2)
+
+    columna1.metric("Palabras", numero_palabras)
+    columna2.metric("Caracteres",numero_caracteres)
